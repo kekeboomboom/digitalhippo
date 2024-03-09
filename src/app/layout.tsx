9990ea1a2805f9338c6fd52789fec968a066dd76
keboom <keboom777@gmail.com>
2024-03-09 16:39:26 +0800

    Add Navbar component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,8 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+
+
 }>) {
   return (
     <html lang="en" className="h-full">
@@ -21,6 +24,7 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           <div className="flex-grow flex-1">{children}</div>
         </main>
       </body>
